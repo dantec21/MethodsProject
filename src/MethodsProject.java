359a1f3663b90da409338af58140f8c8b9066855
Dante <dante.collier.2022@student.lhschools.org>
2019-03-11 14:18:33 -0400

    Second Method

--- src/MethodsProject.java
+++ src/MethodsProject.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 public class MethodsProject {
 
     public static void main(String[] args){
 
         {System.out.println("Hello World");
         }
+        Scanner keyboard = new Scanner(System.in);
+        {
+            System.out.println("What is your name?");
+            String name;
+            name = keyboard.next();
+            System.out.println("Hello " + name);
+        }
+
 
 
 
