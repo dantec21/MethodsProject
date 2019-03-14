036273d3b2a0ef5ca2cb8dee8c120488f944f67b
Dante <dante.collier.2022@student.lhschools.org>
2019-03-14 13:33:52 -0400

    Fifth Method

--- src/MethodsProject.java
+++ src/MethodsProject.java
@@ -15,9 +15,9 @@ public class MethodsProject {
             System.out.println("Hello " + name);
         }
         {
-            System.out.println("Give me a number.");
+            System.out.println("Enter a number.");
             int number1 = keyboard.nextInt();
-            System.out.println("Give me a different number.");
+            System.out.println("Enter a different number.");
             int number2 = keyboard.nextInt();
             System.out.println("The product of the two given numbers is " + (number1 * number2));
         }
@@ -27,6 +27,18 @@ public class MethodsProject {
             System.out.println("Enter a number smaller then the last.");
             int number4 = keyboard.nextInt();
             System.out.println("The qoutient of the two entered numbers is " + (number3/number4));
+        }
+        {
+            System.out.println("Enter a two digit number.");
+            int number5 = keyboard.nextInt();
+            System.out.println("Enter another two digit number.");
+            int number6 = keyboard.nextInt();
+            if(number5>number6){
+                System.out.println(number5 + " is the greatest of the two numbers you entered.");
+            }
+            if(number6>number5){
+                System.out.println(number6 + " is the greatest of the two numbers you entered.");
+            }
         }
         }
 
