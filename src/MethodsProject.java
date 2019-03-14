941f3a947152f552e15ed20758e00728333754a5
Dante <dante.collier.2022@student.lhschools.org>
2019-03-14 13:27:21 -0400

    Fourth Method

--- src/MethodsProject.java
+++ src/MethodsProject.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 
 public class MethodsProject {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        {System.out.println("Hello World");
+        {
+            System.out.println("Hello World");
         }
         Scanner keyboard = new Scanner(System.in);
         {
@@ -13,13 +14,21 @@ public class MethodsProject {
             name = keyboard.next();
             System.out.println("Hello " + name);
         }
-        {System.out.println("Give me a number.");
-        int number1= keyboard.nextInt();
-        System.out.println("Give me a different number.");
-        int number2 = keyboard.nextInt();
-        System.out.println("The product of the two given numbers is "+ (number1*number2));
+        {
+            System.out.println("Give me a number.");
+            int number1 = keyboard.nextInt();
+            System.out.println("Give me a different number.");
+            int number2 = keyboard.nextInt();
+            System.out.println("The product of the two given numbers is " + (number1 * number2));
         }
-
+        {
+            System.out.println("Give me another number.");
+            int number3 = keyboard.nextInt();
+            System.out.println("Enter a number smaller then the last.");
+            int number4 = keyboard.nextInt();
+            System.out.println("The qoutient of the two entered numbers is " + (number3/number4));
+        }
+        }
 
 
 
@@ -33,4 +42,4 @@ public class MethodsProject {
 
 
 
-}
+
